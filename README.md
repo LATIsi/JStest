fbff8ab43af088f5eaf9f24978a1fb394054a8b1
LATIsi <91597005+LATIsi@users.noreply.github.com>
2021-11-09 17:06:20 +0900

    JS 1-9장 배열내장함수까지 완료

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # JStest
+
 Created with CodeSandbox
+
+# 벨로퍼트와 함께하는 모던 JS 공부
+
+https://learnjs.vlpt.us/
+21.11.08부터 시작하여
+현재 1장 반복문까지 완료.
+1~4는 쉬우니 패스
+
+차근차근 배운내용들 정리하기
+객체안에 함수를 넣어도 가능. 그 함수의 this는 포함된 객체를 가르킴
+화살표 함수는 this가 자신이 속한 개체를 가르키지 않고 상위 스코프의 this를 가르키기에 객체안에서 함수 선언시 일반 함수로 선언해야함.
+또한, this 변경 불가능. (call, apply, bind 사용X)
+
+또한 화살표 함수는 생성자 함수 사용 (new 함수.. 재활용) 불가능.
+
+getter / setter
+JAVA에서도 정보 은닉/캡슐화로 배웠던 개념인데,,, JS에서도 가능!
+getter는 값 할당
+setter는 값 반환으로 이해하고있었다..
+
+https://learnjs.vlpt.us/basics/09-array-functions.html
+배열내장함수까지 완료
+
+21.11.09
+프로토타입과 클래스
+undefined 와 null 은 Falsy 한 값
+// null, undefined, 0, '', NaN
--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
-import "./styles.css";
+let name = `사과`;
+let price = 100;
+let num = 5;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+console.log(`${name}의 구매가는 ${price * num}원 입니다. ${5 + 10} 입니다`); // 15 입니다
+
+function hello(name) {
+  return console.log(`안녕하세요, ${name}!`);
+}
+
+hello('민지');
+
+const apple = {
+  name: '사과',
+  color: '빨강',
+  taste: '단맛',
+  price: '500'
+};
+
+const kiwi = {
+  name: '키위',
+  color: 'green',
+  taste: '새콤달콤한 맛',
+  price: '300'
+};
+
+function set({ name, color, taste, price }) {
+  const text = `이 과일의 이름은 ${name}이고, 색은 ${color}이며
+  맛은 무려 ${taste}입니다! 더군다나 지금은 세일해서 원래가격인 ${price}에서 200원을 뺀
+  ${price - 200}원에 구입하실수 있으십니다!`;
+  return console.log(text);
+}
+set(kiwi);
+
+const tweet = {
+  name: '파랑새',
+  color: 'blue',
+  say: '짹짹!',
+  sound: function () {
+    console.log(this.say);
+  }
+};
+
+tweet.sound();
+
+const reduce = [1, 2, 3, 4, 5];
+let sum = reduce.reduce((accumulator, current) => {
+  console.log({ accumulator, current });
+  return accumulator + current;
+}, 0);
+
+console.log(sum);
